01db7ef0dad4e6e0db6b8a6dc77f7aef3359edc0
mradul <mradul.codenote@gmail.com>
2024-09-11 12:10:33 +0530

    add new association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_101944) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_10_105553) do
+  create_table "authors", force: :cascade do |t|
+    t.string "author_name"
+    t.string "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "practice_projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
